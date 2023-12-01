04dbb117c769d6a57ae737e4415c38411615bc95
vcslav-v <donn.nus@gmail.com>
2023-12-01 11:08:37 +0100

    fix

--- pb_portal/static/js/bulk_tagging.js
+++ pb_portal/static/js/bulk_tagging.js
@@ -172,6 +172,7 @@ const changepage = function (delta) {
 const set_search = function (url, page_html_url, img_url, add_tag_url, refresh_active_tasks_url, search_tag_url, main_site_url) {
     var search_form = document.getElementById('search_form');
     var tag_input = document.getElementById('tag_input');
+    $('#load_spin').hide();
     _add_tag_url = add_tag_url;
     _refresh_active_tasks_url = refresh_active_tasks_url;
     _search_tag_url = search_tag_url;
@@ -179,6 +180,10 @@ const set_search = function (url, page_html_url, img_url, add_tag_url, refresh_a
     refresh_active_tasks();
     search_form.onsubmit = function (e) {
         e.preventDefault();
+        var page = $('#products_list');
+        page.empty();
+        $('#load_spin').show();
+        
         var formData = new FormData(search_form);
         $.ajax({
             type: 'POST',
@@ -198,6 +203,7 @@ const set_search = function (url, page_html_url, img_url, add_tag_url, refresh_a
                 _page_html_url = page_html_url;
                 cur_category_name = response.category_name;
                 cur_category_id = response.category_id;
+                $('#load_spin').hide();
                 show_page()
                 load_imgs();
             },
@@ -222,8 +228,7 @@ const add_tag = function () {
     );
     formData.append('category_id', cur_category_id);
     formData.append('disable_cards', JSON.stringify(disable_cards));
-    formData.append('tag', $('#modal_tag_name').val());
-    formData.append('description', $('#modal_tag_description').val());
+    formData.append('tag', $('input[name="tag"]').val());
     $.ajax({
         type: 'POST',
         url: _add_tag_url,
