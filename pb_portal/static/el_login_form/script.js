dd3267f180abb8852f3a3df69356eb55aaf15259
vcslav-v <donn.nus@gmail.com>
2024-05-23 18:02:15 +0200

    login form upd

--- pb_portal/static/el_login_form/script.js
+++ pb_portal/static/el_login_form/script.js
@@ -3,10 +3,10 @@ function switchVisiblePassword() {
     var img = document.getElementById("password-eye");
     if (img.src === img.getAttribute("data-open-eye-src")) {
         img.src = img.getAttribute("data-closed-eye-src");
-        passwordInput.type = "password";
+        passwordInput.type = "text";
     } else {
         img.src = img.getAttribute("data-open-eye-src");
-        passwordInput.type = "text";
+        passwordInput.type = "password";
     }
 }
 
@@ -22,9 +22,17 @@ function submitForm(event) {
         .catch(function (error){
             if (error.response.status === 400 && error.response.data.message === 'LOGIN_BAD_CREDENTIALS') {
                 const inputs = document.getElementsByTagName('input');
+                const error_msg = document.getElementsByClassName('login-form__error')[0];
                 for (let i = 0; i < inputs.length; i++) {
                     inputs[i].classList.add('bad-credentials');
                 }
+                error_msg.style.display = 'block';
             }
         })
+}
+
+function toDefault(event) {
+    if (event.target.classList.contains('bad-credentials')){
+        event.target.classList.remove('bad-credentials');
+    };
 }
