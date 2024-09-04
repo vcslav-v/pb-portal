e19bdff238df3f83dce62d3e6af978d4c4e01b60
vcslav-v <donn.nus@gmail.com>
2024-09-04 12:18:14 +0200

    fix ar

--- pb_portal/imgs.py
+++ pb_portal/imgs.py
@@ -5,16 +5,19 @@ from pb_admin import schemas as pb_schemas
 import requests
 
 
-def prepare_pb_preview_image(image: bytes, filename: str) -> tuple[io.BytesIO | None, io.BytesIO | None, str, str]:
+def prepare_pb_preview_image(
+    image: bytes,
+    filename: str,
+) -> tuple[io.BytesIO | None, io.BytesIO | None, str, str]:
     img = Image.open(io.BytesIO(image))
     if img.width < config.MIN_PB_IMAGE_WIDTH or img.height < config.MIN_PB_IMAGE_HEIGHT:
         return None, None, 'Image has to be at least 2080x1386 pixels', filename
 
-    aspect_ratio = img.width / img.height
-    if round(aspect_ratio, 2) != round(config.PB_IMAGE_ASPECT_RATIO, 2):
-        return None, None, 'Aspect ratio has to be 3/2', filename
+    # aspect_ratio = img.width / img.height
+    # if round(aspect_ratio, 2) != round(config.PB_IMAGE_ASPECT_RATIO, 2):
+    #     return None, None, 'Aspect ratio has to be 3/2', filename
 
-    img.thumbnail((config.MAX_IMAGE_WIDTH, config.MAX_IMAGE_HEIGHT))
+    img.thumbnail((config.MAX_IMAGE_WIDTH, img.height))
     img_bytes = io.BytesIO()
     img.save(img_bytes, format='JPEG')
     img_bytes.seek(0)
