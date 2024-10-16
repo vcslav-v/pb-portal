eb4139dc5f759b61fd7bcbb69363d2dd2bd408ad
vcslav-v <donn.nus@gmail.com>
2024-10-16 15:52:57 +0200

    upd

--- pb_portal/routes/api/sendy.py
+++ pb_portal/routes/api/sendy.py
@@ -8,7 +8,7 @@ router = APIRouter()
 
 @config.logger.catch()
 @router.post('/unsubscribe')
-async def help(
+async def unsubscribe(
     request: Request,
 ):
     body_bytes = await request.body()
@@ -18,3 +18,17 @@ async def help(
     if email:
         email = email[0]
         sendy.add_unsubscribe_list(email)
+
+
+@config.logger.catch()
+@router.post('/subscribe')
+async def subscribe(
+    request: Request,
+):
+    body_bytes = await request.body()
+    body_str = body_bytes.decode('utf-8')
+    body_dict = parse_qs(body_str)
+    email = body_dict.get('email', None)
+    if email:
+        email = email[0]
+        sendy.rm_unsubscribe_list(email)
--- pb_portal/sendy.py
+++ pb_portal/sendy.py
@@ -12,3 +12,15 @@ def add_unsubscribe_list(email: str):
         config.SENDY_API_URL.format(action='subscribe'),
         data=data
     )
+
+
+def rm_unsubscribe_list(email: str):
+    data = {
+        'api_key': config.SENDY_API_KEY,
+        'list_id': config.UNSUBSCRIBERS_LIST_ID,
+        'email': email,
+    }
+    requests.post(
+        config.SENDY_API_URL.format(action='api/subscribers/delete.php'),
+        data=data
+   )
