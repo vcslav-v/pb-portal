dd75f99f1f35edec61757edcd15cf1176abafdfe
vcslav-v <donn.nus@gmail.com>
2024-10-16 10:07:22 +0200

    upd

--- pb_portal/routes/api/sendy.py
+++ pb_portal/routes/api/sendy.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Request
 
 from pb_portal import config
 from pb_portal.schemas.sendy import Unsubscribe
+from urllib.parse import parse_qs
 
 router = APIRouter()
 
@@ -11,4 +12,7 @@ router = APIRouter()
 async def help(
     request: Request,
 ):
-    config.logger.info(await request.body())
+    body_bytes = await request.body()
+    body_str = body_bytes.decode('utf-8')
+    body_dict = parse_qs(body_str)
+    config.logger.info(body_dict)
